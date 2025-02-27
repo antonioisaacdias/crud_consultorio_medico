99c1003d6ee51733699e4489b6d065d0cc627ebe
Antonio Dias <antonioisaacvd@gmail.com>
2025-02-27 17:41:26 -0300

    :sparkles: feat: view de operações CRUD do model Patient

--- backend/urls.py
+++ backend/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('professionals/', views.professionals, name='professionals'),
-    path('professionals/<uuid:pk>/', views.professionals_detail, name='professionals_detail')
+    path('professionals/<uuid:pk>/', views.professionals_detail, name='professionals_detail'),
+    path('patients/', views.patients, name='patients'),
+    path('patients/<uuid:pk>/', views.patients_detail, name='patient_detail')
 ]
--- backend/views.py
+++ backend/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Professional, Patient, Appointment
-from .serializers import ProfessionalSerializer
+from .serializers import ProfessionalSerializer, PatientSerializer
 
 @api_view(['GET', 'POST'])
 def professionals(request):
@@ -14,7 +14,7 @@ def professionals(request):
         serializer = ProfessionalSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'message' : 'Cadastrado com sucesso!'}, status=status.HTTP_201_CREATED)
+            return Response({'message' : 'Médico cadastrado com sucesso!'}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PATCH', 'DELETE'])
@@ -34,9 +34,45 @@ def professionals_detail(request, pk):
         serializer = ProfessionalSerializer(professional, data=request.data, partial=True)
         
         if serializer.is_valid():
-            updated_professional = serializer.update(professional, serializer.validated_data)
+            serializer.update(professional, serializer.validated_data)
             return Response({'message': 'Informações atualizadas com sucesso!'}, status=status.HTTP_200_OK)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'POST'])
+def patients(request):
+    if request.method == 'GET':
+        patients = Patient.objects.all()
+        serializer = PatientSerializer(patients, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = PatientSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message' : 'Paciente cadastrado com sucesso!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PATCH','DELETE'])
+def patients_detail(request, pk):
+    if request.method == 'GET':
+        patient = Patient.objects.get(id=pk)
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        patient = Patient.objects.get(id=pk)
+        patient.delete()
+        return Response({'message': 'Excluído com sucesso!'}, status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PATCH':
+        patient = Patient.objects.get(id=pk)
+        serializer = PatientSerializer(patient, data=request.data, partial=True)
+        
+        if serializer.is_valid():
+            serializer.update(patient, serializer.validated_data)
+            return Response({'message': 'Informações atualizadas com sucesso!'}, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
