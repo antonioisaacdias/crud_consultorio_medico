3132f19696e5292cd2492891f9bfd4ba948837e1
Antonio Dias <antonioisaacvd@gmail.com>
2025-02-27 10:57:20 -0300

    :construction: Models do backend

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class Professional(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(unique=True)
+    crm = models.CharField(max_length=10 ,unique=True, null=False, blank=False)
+    specialty = models.CharField(max_length=80, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class Patient(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(unique=True)
+    address = models.TextField()
+    phone = models.CharField(max_length=11, null=False, blank=False)
+    cpf = models.CharField(max_length=11, unique=True, blank=False, null=False)
+    birth_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class Appointment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    appointment_datetime = models.DateTimeField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    professional = models.ForeignKey(Professional, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    
